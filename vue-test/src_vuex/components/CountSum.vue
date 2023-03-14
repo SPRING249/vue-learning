41c94f796b2c05c9a1293d1b4f4d8e12f053f0bc
pbw <2106107257@qq.com>
2023-03-14 13:35:47 +0800

    2023/3/14

---
+++ vue-test/src_vuex/components/CountSum.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>当前求和为：{{ sum }}</h1>
+    <h3>当前求和放大10倍：{{ bigSum }}</h3>
+    <h3>我在{{ school }},学习{{ subject }}</h3>
+    <select v-model.number="n">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+
+export default {
+  name: "CountSum",
+  data() {
+    return {
+      n: 1, //用户选择的数字
+    }
+  },
+  computed: {
+    //拓展运算符
+    ...mapState(['sum', 'school', 'subject']),
+    ...mapGetters(['bigSum'])
+  },
+  methods: {
+    //递增,使用commit触发Mutation操作
+    increment() {
+      this.$store.commit('ADD', this.n)
+    },
+    //递减
+    decrement() {
+      this.$store.commit('SUBTRACT', this.n)
+    },
+    //增量奇数,直接使用dispatch触发action函数
+    incrementOdd() {
+      this.$store.dispatch('addOdd', this.n)
+    },
+    //增量等待
+    incrementWait() {
+      this.$store.dispatch('addWait', this.n)
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-test/src_求和案例_mapxx/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <count-sum></count-sum>
+  </div>
+
+</template>
+<script>
+import CountSum from "@/components/CountSum.vue";
+
+export default {
+  name: "App",
+  components: {
+    CountSum
+  }
+}
+</script>
+<style>
+button {
+  margin-left: 5px;
+}
+</style>
